9b835e43be7af64e6f347c6af32c2aea1c7f28f7
Eric Eslinger <eric.eslinger@gmail.com>
2017-03-20 14:39:53 -0700

    woops

--- src/rest.js
+++ src/rest.js
@@ -4,7 +4,7 @@ import { JSONApi } from 'plump-json-api';
 
 const $axios = Symbol('$axios');
 const $json = Symbol('$json');
-import Promise from 'bluebird';
+import Bluebird from 'bluebird';
 
 export class RestStore extends Storage {
   constructor(opts = {}) {
@@ -25,38 +25,42 @@ export class RestStore extends Storage {
     return this[$axios](options);
   }
 
-  write(t, v) {
-    return Promise.resolve()
+  write(typeName, v) {
+    const type = this.getType(typeName);
+    return Bluebird.resolve()
     .then(() => {
-      if (v[t.$id]) {
-        return this[$axios].patch(`/${t.$name}/${v[t.$id]}`, v);
+      if (v.id) {
+        return this[$axios].patch(`/${type.$name}/${v.id}`, v);
       } else if (this.terminal) {
-        return this[$axios].post(`/${t.$name}`, v);
+        return this[$axios].post(`/${type.$name}`, v);
       } else {
         throw new Error('Cannot create new content in a non-terminal store');
       }
     })
     .then((response) => {
-      const result = this[$json].parse(response.data);
-      result.extended.forEach((item, index) => {
-        const schema = this[$json].schema(item.type);
-        const childRelationships = response.data.included[index].relationships;
-        this.notifyUpdate(schema, item.id, item, Object.keys(childRelationships).concat($self));
-      });
-      const root = {};
-      for (const field in result.root) {
-        if (field !== 'type') {
-          root[field] = result.root[field];
-        }
-      }
-      return root;
+      const result = this[$json].parse(response.data).data;
+      return result;
+      // return Bluebird.all(Object.keys(type.relationships).map((relName) => {
+      //   if (v.relationships && v.relationships[relName]) {
+      //     return Bluebird.all(v.relationships[relName].map((delta) => {
+      //       if (delta.op === 'add') {
+      //         return this.add(typeName, result.id, relName, delta.id, delta.meta)
+      //       }
+      //     }))
+      //   }
+      // }))
+      // result.extended.forEach((item, index) => {
+        // const schema = this[$json].schema(item.type);
+        // const childRelationships = response.data.included[index].relationships;
+        // this.notifyUpdate(schema, item.id, item, Object.keys(childRelationships).concat($self));
+      // });
     })
-    .then((result) => this.notifyUpdate(t, result[t.$id], result).then(() => result));
+    .then((result) => this.notifyUpdate(typeName, result.id, result).then(() => result));
   }
 
-  readOne(t, id) {
-    return Promise.resolve()
-    .then(() => this[$axios].get(`/${t.$name}/${id}`))
+  readOne(typeName, id) {
+    return Bluebird.resolve()
+    .then(() => this[$axios].get(`/${typeName}/${id}`))
     .then((response) => {
       const result = this[$json].parse(response.data);
       result.extended.forEach((item, index) => {
@@ -80,8 +84,8 @@ export class RestStore extends Storage {
     });
   }
 
-  readMany(t, id, relationship) {
-    return this[$axios].get(`/${t.$name}/${id}/${relationship}`)
+  readMany(typeName, id, relationship) {
+    return this[$axios].get(`/${typeName}/${id}/${relationship}`)
     .then((response) => response.data)
     .catch((err) => {
       if (err.response && err.response.status === 404) {
@@ -92,31 +96,24 @@ export class RestStore extends Storage {
     });
   }
 
-  add(type, id, relationshipTitle, childId, extras) {
-    const relationshipBlock = type.$fields[relationshipTitle];
-    const sideInfo = relationshipBlock.relationship.$sides[relationshipTitle];
-    const newField = { [sideInfo.self.field]: id, [sideInfo.other.field]: childId };
-    if (relationshipBlock.relationship.$extras) {
-      Object.keys(relationshipBlock.relationship.$extras).forEach((extra) => {
-        newField[extra] = extras[extra];
-      });
-    }
-    return this[$axios].put(`/${type.$name}/${id}/${relationshipTitle}`, newField)
-    .then(() => this.notifyUpdate(type, id, null, relationshipTitle));
+  add(typeName, id, relationshipTitle, childId, extras) {
+    const newField = { id: childId, meta: Object.assign({}, extras) };
+    return this[$axios].put(`/${typeName}/${id}/${relationshipTitle}`, newField)
+    .then(() => this.notifyUpdate(typeName, id, null, relationshipTitle));
   }
 
-  remove(t, id, relationshipTitle, childId) {
-    return this[$axios].delete(`/${t.$name}/${id}/${relationshipTitle}/${childId}`)
-    .then(() => this.notifyUpdate(t, id, null, relationshipTitle));
+  remove(typeName, id, relationshipTitle, childId) {
+    return this[$axios].delete(`/${typeName}/${id}/${relationshipTitle}/${childId}`)
+    .then(() => this.notifyUpdate(typeName, id, null, relationshipTitle));
   }
 
-  modifyRelationship(t, id, relationshipTitle, childId, extras) {
-    return this[$axios].patch(`/${t.$name}/${id}/${relationshipTitle}/${childId}`, extras)
-    .then(() => this.notifyUpdate(t, id, null, relationshipTitle));
+  modifyRelationship(typeName, id, relationshipTitle, childId, extras) {
+    return this[$axios].patch(`/${typeName}/${id}/${relationshipTitle}/${childId}`, extras)
+    .then(() => this.notifyUpdate(typeName, id, null, relationshipTitle));
   }
 
-  delete(t, id) {
-    return this[$axios].delete(`/${t.$name}/${id}`)
+  delete(typeName, id) {
+    return this[$axios].delete(`/${typeName}/${id}`)
     .then((response) => {
       return response.data;
     });
